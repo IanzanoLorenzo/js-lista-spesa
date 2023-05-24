d2fc6fc2e5518597c03984bf29b2045d2edbb9bc
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-24 14:52:56 +0200

    exercise done

--- script/script.js
+++ script/script.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const listaSpesa = document.getElementById('lista-spesa');
+let lista = [
+    'Pane',
+    'Latte',
+    'Fiori',
+    'Piatti di plastica',
+    'Biscotti',
+    'Uova',
+    'Farina',
+    'Medicine'
+];
+
+let counter = 0;
+
+while (counter < lista.length){
+    let addElement = document.createElement('li')
+    addElement.innerHTML = lista[counter]
+    listaSpesa.append(addElement)
+    counter++
+}
