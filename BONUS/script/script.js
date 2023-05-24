0ad4f498611f25cc894085433727d904d0c1acfe
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-05-24 15:27:53 +0200

    bonus done: background color casuale

--- BONUS/script/script.js
+++ BONUS/script/script.js
@@ -7,23 +7,28 @@ let lista = [
     'Fiori',
     'Piatti di plastica',
     'Biscotti',
-    'Tritolo',
-    'Meth'
+    'Uova',
+    'Farina',
+    'Medicine',
+    'Prosciutto',
+    'Yogurt'
 ];
 
 let col = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e','f']
 
+let color = '#'
 let counter = 0;
+while(counter < 6){
+    let index = Math.floor(Math.random() * 15)
+    color += col[index]
+    console.log(color)
+    counter++
+}
 
+counter = 0;
 while (counter < lista.length){
     let addElement = document.createElement('li')
-    let color = '#'
-    for(let i = 0; i < 6;i++){
-        let index = Math.floor(Math.random() * 15)
-        color += col[index]
-    }
-    addElement.innerHTML =`<div style="color: ${color}" >${lista[counter]} colore:${color}</div>` 
+    addElement.innerHTML =`<div style="background-color: ${color}" ><label>${lista[counter]}</label></div>` 
     listaSpesa.append(addElement)
-    console.log(color)
     counter++
 }
